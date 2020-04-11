7553ce5f76a7d16dc1f342d57ff80801fd47928b
Léo Peltier <contact@leo-peltier.fr>
2020-04-11 17:08:45 +0200

    Update run instructions

--- main.go
+++ main.go
@@ -4,8 +4,12 @@ import (
 	"flag"
 	"fmt"
 	"kaepora/internal/back"
+	"kaepora/internal/bot"
 	"log"
 	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -61,4 +65,29 @@ COMMANDS
 `,
 		os.Args[0],
 	)
+}
+
+func serve(b *back.Back) error {
+	done := make(chan struct{})
+	signaled := make(chan os.Signal, 1)
+	signal.Notify(signaled, syscall.SIGINT, syscall.SIGTERM)
+
+	bot, err := bot.New(b, os.Getenv("KAEPORA_DISCORD_TOKEN"))
+	if err != nil {
+		return err
+	}
+
+	var wg sync.WaitGroup
+	go b.Run(&wg, done)
+	go bot.Serve(&wg, done)
+
+	sig := <-signaled
+	log.Printf("warning: received signal %d", sig)
+	close(done)
+
+	log.Print("info: waiting for complete shutdown")
+	wg.Wait()
+	log.Print("info: shutdown complete")
+
+	return nil
 }
