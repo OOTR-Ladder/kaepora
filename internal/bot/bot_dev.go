64626dd591032d7598c00317e3e9ff4227dcc41a
Léo Peltier <contact@leo-peltier.fr>
2020-04-29 16:32:37 +0200

    Bring back dev help

--- internal/bot/bot_dev.go
+++ internal/bot/bot_dev.go
@@ -11,12 +11,27 @@ import (
 	"github.com/google/uuid"
 )
 
+// nolint:funlen
 func (bot *Bot) cmdDev(m *discordgo.Message, args []string, out io.Writer) error {
 	if m.Author.ID != bot.adminUserID {
 		return fmt.Errorf("!dev command ran by a non-admin: %v", args)
 	}
 	if len(args) < 1 {
-		return util.ErrPublic("need a subcommand")
+		fmt.Fprintf(out, `
+**Admin-only commands**:
+%[1]s
+!dev closesession            # close the debug race
+!dev createsession SHORTCODE # create a new debug race starting immediately
+!dev error                   # error out
+!dev panic                   # panic and abort
+!dev seed SHORTCODE [SEED]   # generate a seed valid for the given league
+!dev setannounce SHORTCODE   # configure a league to post its announcements in the channel the command was sent in
+!dev uptime                  # display for how long the server has been running
+!dev url                     # display the link to use when adding the bot to a new server
+%[1]s`,
+			"```",
+		)
+		return nil
 	}
 
 	switch args[0] { // nolint:gocritic, TODO
@@ -60,7 +75,7 @@ func (bot *Bot) cmdDev(m *discordgo.Message, args []string, out io.Writer) error
 			return util.ErrPublic("expected 1 argument: SHORTCODE")
 		}
 		return bot.back.CreateDevMatchSession(args[1])
-	case "closesession": // SHORTCODE
+	case "closesession":
 		return bot.back.CloseDevMatchSession()
 	default:
 		return util.ErrPublic("invalid command")
