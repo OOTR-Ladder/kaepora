70f99f13e615f34dc48a9d65b06f6b89dbd8f9d9
Léo Peltier <contact@leo-peltier.fr>
2020-06-12 13:05:59 +0200

    Allow players to set their stream URL

--- internal/bot/bot_self.go
+++ internal/bot/bot_self.go
@@ -13,15 +13,26 @@ func (bot *Bot) cmdRename(m *discordgo.Message, args []string, out io.Writer) er
 		return util.ErrPublic("your forgot to tell me your desired name")
 	}
 
-	return bot.setDiscordPlayerName(m.Author.ID, argsAsName(args), out)
-}
-
-func (bot *Bot) setDiscordPlayerName(discordID string, name string, out io.Writer) error {
-	if err := bot.back.UpdateDiscordPlayerName(discordID, name); err != nil {
+	name := argsAsName(args)
+	if err := bot.back.UpdateDiscordPlayerName(m.Author.ID, name); err != nil {
 		return err
 	}
 
 	fmt.Fprintf(out, "You'll be henceforth known as `%s` on the leaderboards.", name)
+	return nil
+}
+
+func (bot *Bot) cmdSetStream(m *discordgo.Message, args []string, out io.Writer) error {
+	if len(args) < 1 {
+		return util.ErrPublic("your forgot to tell me your stream URL")
+	}
+
+	url, err := bot.back.UpdateDiscordPlayerStreamURL(m.Author.ID, argsAsName(args))
+	if err != nil {
+		return err
+	}
+
+	fmt.Fprintf(out, "Your stream URL has been set to %s", url)
 	return nil
 }
 
