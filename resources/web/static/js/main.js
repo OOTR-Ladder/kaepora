091ac65937f3895831c9aa07c9ddbc109fe19224
François WAXIN <francoiswax@gmail.com>
2020-09-01 21:55:18 +0200

    Grouping each league stats in one tabbed block + Allowing multiple tabs blocks to appear in the same page without conflicting to eachother.

--- resources/web/static/js/main.js
+++ resources/web/static/js/main.js
@@ -1,5 +1,43 @@
 "use strict";
 
+function tabsWithContent(container) {
+    let myID = document.getElementById(container);
+
+    let tabs = myID.querySelectorAll('.tabs li');
+    let tabsContent = myID.querySelectorAll('.tab-content');
+  
+    let deactvateAllTabs = function () {
+      tabs.forEach(function (tab) {
+        tab.classList.remove('is-active');
+      });
+    };
+  
+    let hideTabsContent = function () {
+      tabsContent.forEach(function (tabContent) {
+        tabContent.classList.remove('is-active');
+      });
+    };
+  
+    let activateTabsContent = function (tab) {
+      tabsContent[getIndex(tab)].classList.add('is-active');
+    };
+  
+    let getIndex = function (el) {
+      return [...el.parentElement.children].indexOf(el);
+    };
+  
+    tabs.forEach(function (tab) {
+      tab.addEventListener('click', function () {
+        deactvateAllTabs();
+        hideTabsContent();
+        tab.classList.add('is-active');
+        activateTabsContent(tab);
+      });
+    })
+  
+    tabs[0].click();
+  };
+
 window.Kaepora = {
     bindSortableTables(tableQuery) {
         const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
---
+++ resources/web/static/js/one-player.js
@@ -0,0 +1,6 @@
+"use strict";
+
+(function (){
+    tabsWithContent("PlayerPerformances");
+    tabsWithContent("raceList");
+})();
